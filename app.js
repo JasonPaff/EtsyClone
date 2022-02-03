1957c36b9f4828f0575c1f0bb6c1f3746419b985
Jason Paff <jasonpaff@gmail.com>
2022-02-03 15:59:33 -0500

    updated project template

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const logger = require('morgan');
 const path = require("path");
+const debug = require('debug')('etsyclone:server');
+const http = require('http');
+const port = process.env.PORT || '3000';
+
 const app = express();
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
 const indexRoute = require('./routes/index.js');
 const loginRoute = require('./routes/login.js');
@@ -19,4 +30,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", indexRoute);
 app.use("/login", loginRoute);
 
-module.exports = app;
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
