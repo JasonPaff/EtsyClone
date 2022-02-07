a30f522b50f809ea3b9927062fe9d743f683e061
Jason Paff <jasonpaff@gmail.com>
2022-02-06 21:10:07 -0500

    fixed database store association

--- models/store.js
+++ models/store.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.Store.hasOne(models.User, { as: 'user', foreignKey: 'store_id' })
+      models.Store.hasOne(models.User, { as: 'user', foreignKey: 'id' })
     }
   }
   Store.init({
