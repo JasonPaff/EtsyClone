9756ad2c5d9f6572fe4a55df2f77e0ebcd3a2320
Jason Paff <jasonpaff@gmail.com>
2022-02-07 15:00:55 -0500

    store/product pages

--- routes/store.js
+++ routes/store.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const models = require("../models");
 const router = express.Router();
 
 // TODO: load real products from database
@@ -19,5 +20,30 @@ const products = [{
 router.get('/', function (req, res) {
     res.render('store', {title: 'Etsy Clone', loggedIn: req.session.loggedIn, products: products});
 });
+
+router.post('/', function (req, res) {
+    getStoreProducts(req, res).catch(console.error);
+});
+
+// get all the stores from the database
+async function getStoreProducts(req, res) {
+    const products = await models.Product.findAll({
+        where: {
+            user_id: req.body.userId
+        }
+    })
+
+    const store = await models.Store.findOne({
+        where: {
+            user_id: req.body.userId
+        }
+    })
+
+    products.forEach(product => product.salePercent = ((1 - (product.sale_price / product.price)) * 100).toFixed(2))
+
+    res.render('store', {
+        title: 'Etsy Clone', loggedIn: req.session.loggedIn, products: products, store_name: store.store_name
+    });
+}
 
 module.exports = router;
