e864ce73173b04f82d6916da8f88a5711a36e610
Phil-Slater <pdsltr@gmail.com>
2022-02-07 15:23:05 -0500

    add isAdmin boolean to users table

---
+++ migrations/20220207202004-add admin boolean to Users table.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('Users', 'isAdmin', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
