efb456ce8068e1a6316fd137cf3622158ed1ebd3
Jason Paff <jasonpaff@gmail.com>
2022-02-09 16:10:13 -0500

    search bar

--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.render('search', {title: 'Etsy Clone', session: req.session });
+router.post('/', function (req, res) {
+    searchProducts(req, res).catch(console.error);
+    //res.render('search', {title: 'Etsy Clone', session: req.session });
 });
+
+async function searchProducts(req, res) {
+    const products = await require('../utils/dbUtils').getAllProductsByKeyword(req.body.searchBar);
+    const adjustedProducts = require('../utils/dbUtils').calculateSalePrices(products);
+    res.render('products', { title: 'Etsy Clone', session: req.session, products: adjustedProducts });
+}
 
 module.exports = router;
