73306bbcb1fc5c97235c4f570b1711a9932c17f2
Jason Paff <jasonpaff@gmail.com>
2022-02-09 19:49:23 -0500

    checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -2,7 +2,48 @@
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('checkout', {title: 'Etsy Clone', session: req.session});
+    getCart(req, res).catch(console.error);
 });
+
+router.post('/removeFromCart', function (req, res) {
+    removeFromCart(req, res).catch(console.error);
+});
+
+// removes an item from the cart
+async function removeFromCart(req, res) {
+    if (req.session.loggedIn) {
+        await require('../utils/dbUtils').removeProductFromCart(req.body.productId, req.session.user);
+        req.session.cartCount = await require('../utils/dbUtils').getCartCount(req.session.user);
+        getCart(req, res).catch(console.error);
+    }
+}
+
+// renders the cart contents to the cart page
+async function getCart(req, res) {
+    let products = null;
+
+    const cart = await require('../utils/dbUtils').getUserCart(req.session.user);
+    products = await require('../utils/dbUtils').getAllCartProducts(cart);
+
+    let subTotal = 0;
+
+    if (products !== null) {
+        products.forEach(product => {
+            subTotal += parseFloat(product.dataValues.price);
+        });
+    }
+
+    const tax = subTotal * 0.05;
+    const total = subTotal + tax;
+
+    res.render('checkout', {
+        title: 'Checkout',
+        session: req.session,
+        products: products,
+        subTotal: subTotal.toFixed(2),
+        tax: tax.toFixed(2),
+        total: total.toFixed(2),
+    });
+}
 
 module.exports = router;
